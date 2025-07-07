72ddb4cbcd86641ae246624008879e3fb79845d8
gengyi <junjunxu92@gmail.com>
2025-07-07 14:42:23 +0800

    change

--- src/packages/product/pages/publish.tsx
+++ src/packages/product/pages/publish.tsx
@@ -5,17 +5,46 @@ import {
   Text,
   Image,
   ScrollView,
-  Input
+  Input,
+  Swiper,
+  SwiperItem
 } from '@tarojs/components';
 import fetch from '@/lib/request';
 import styles from './publish.module.scss';
 import SearchIcon from '@/assets/generation/search.png';
 import FilterIcon from '@/assets/generation/filter.png';
 
+definePageConfig({
+  navigationBarTitleText: "产品库", // 会被动态覆盖
+  navigationStyle:"default",
+  disableScroll: true,
+  enableShareAppMessage: true,
+  enableShareTimeline: true
+});
+
 const productPublish = () => {
-    return (
-        <View></View>
-    )
+  return (
+    <View className={styles['product_publish']}>
+      <ScrollView
+        className={styles['product_publish_main']}
+        scrollY={true}
+      >
+
+      </ScrollView>
+      <View className={styles['product_publish_action']}>
+        <View
+          className={`${styles['product_publish_action_item']} ${styles['cancel']}`}
+        >
+          取消
+        </View>
+        <View
+          className={`${styles['product_publish_action_item']} ${styles['sure']}`}
+        >
+          保存并返回
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default productPublish();
